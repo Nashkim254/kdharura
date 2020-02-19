805b2265bebff09832ba9ab4e72d2c33e8694680
programming6131 <kimutainashon@gmail.com>
2020-02-19 14:22:29 +0300

    third recommit

--- lib/app_screens/contacts.dart
+++ lib/app_screens/contacts.dart
@@ -2,65 +2,71 @@ import 'package:flutter/material.dart';
 
 class Contacts extends StatelessWidget {
   final String title;
+
   Contacts(this.title);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Contacts',
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        accentColor: Colors.teal,
-        primaryColor: Colors.cyanAccent
-      ),
-      home: Home(),
-    );
-  }
-}
-
-class Home extends StatefulWidget {
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back),
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          ),
-          title: Text("Contacts", style: TextStyle(color: Colors.white),),
-        ),
-        body: Center(
-            child: ListView(
+        debugShowCheckedModeBanner: false,
+        title: 'Contacts',
+        theme: ThemeData(
+            brightness: Brightness.dark,
+            accentColor: Colors.teal,
+            primaryColor: Colors.cyanAccent),
+        home: Scaffold(
+            appBar: AppBar(
+              leading: IconButton(
+                icon: Icon(Icons.arrow_back),
+                onPressed: () {
+                  Navigator.of(context).pushNamed("/");
+                },
+              ),
+              title: Text(
+                "Contacts",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            body: Center(
+                child: ListView(
               children: <Widget>[
                 ListTile(
-                  title: Text('Email:kdharura@gmail.com', style: TextStyle(color: Colors.white)),
+                  title: Text('Email:kdharura@gmail.com',
+                      style: TextStyle(color: Colors.white)),
                   trailing: Icon(Icons.email),
                 ),
-                Divider(thickness: 5.0,),
-                ListTile(
-                  title: Text('Telephone:+254724821720', style: TextStyle(color: Colors.white),),
-                  trailing: Icon(Icons.phone),
+                Divider(
+                  thickness: 5.0,
                 ),
-                Divider(thickness: 5.0,),
                 ListTile(
                   title: Text(
-                    'Website: www.kimutainshonwebhostapp.com', style: TextStyle(color: Colors.white),
+                    'Telephone:+254724821720',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  trailing: Icon(Icons.phone),
+                ),
+                Divider(
+                  thickness: 5.0,
+                ),
+                ListTile(
+                  title: Text(
+                    'Website: www.kimutainshonwebhostapp.com',
+                    style: TextStyle(color: Colors.white),
                   ),
                   trailing: Icon(Icons.web),
                 ),
-                Divider(thickness: 5.0,),
+                Divider(
+                  thickness: 5.0,
+                ),
                 ListTile(
-                  title: Text('Cell: 0716367358', style: TextStyle(color: Colors.white)),
+                  title: Text('Cell: 0716367358',
+                      style: TextStyle(color: Colors.white)),
                   trailing: Icon(Icons.call),
                 ),
-                Divider(thickness: 5.0,),
+                Divider(
+                  thickness: 5.0,
+                ),
               ],
-            )));
+            ))));
   }
 }
